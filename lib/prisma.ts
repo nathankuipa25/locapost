5adb8de3c45c7ce4803371c60230bd0a45b2ebbb
nathankuipa25 <nathankuipa44@gmail.com>
2026-08-18 22:42:50 +0200

    change Prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/app/generated/prisma/client";
 
 const connectionString = process.env.DATABASE_URL;
 
